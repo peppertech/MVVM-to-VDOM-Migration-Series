3cbde2be81605f76c0884af37aaa359ed5b184ba
peppertech@gmail.com <peppertech@gmail.com>
2022-06-03 15:37:54 -0700

    adding asset files and local data file

--- scripts/hooks/after_build.js
+++ scripts/hooks/after_build.js
@@ -5,11 +5,44 @@
 
 */
 
-'use strict';
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const archiver = require("archiver");
 
 module.exports = function (configObj) {
   return new Promise((resolve, reject) => {
-  	console.log("Running after_build hook.");
-  	resolve(configObj);
+    console.log("Running after_build hook.");
+    //create dist folder at root of project to hold the archive
+    fs.mkdir(path.join(__dirname, "../../dist"), { recursive: true }, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("Directory created successfully!");
+    });
+
+    //change the extension of the my-archive.xxx file from .zip to .war as needed
+    const output = fs.createWriteStream("./dist/my-archive.zip");
+    //leave unchanged, compression is the same for WAR or Zip file
+    const archive = archiver("zip");
+
+    output.on("close", () => {
+      console.log("Files were successfully archived.");
+      resolve();
+    });
+
+    archive.on("warning", (error) => {
+      console.warn(error);
+    });
+
+    archive.on("error", (error) => {
+      reject(error);
+    });
+
+    archive.pipe(output);
+    archive.directory("web", false);
+    archive.finalize();
+
+    resolve(configObj);
   });
 };
