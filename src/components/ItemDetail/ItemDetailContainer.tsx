1cc04f2aa61da8dcf03f0d06f9baf84eb769c63c
peppertech@gmail.com <peppertech@gmail.com>
2022-06-12 08:01:07 -0700

    adding redux toolkit example

--- src/components/ItemDetail/ItemDetailContainer.tsx
+++ src/components/ItemDetail/ItemDetailContainer.tsx
@@ -1,18 +1,34 @@
 import { h } from "preact";
 import { KeySetImpl, KeySet } from "ojs/ojkeyset";
+import { useStore, useSelector } from "react-redux";
 
 type Props = {
   item?: KeySetImpl<string>;
 };
 
 const ItemDetailContainer = (props: Props) => {
+  const store: any = useStore();
+  const state = store.getState();
+  const messageState = useSelector(() => {
+    return {...state, 
+      message:state.message
+    }
+  });
+
+  const updateMessage = () => {
+    store.dispatch({
+      type: "message/update",
+      message: "New message from Redux!",
+    });
+  };
+  
   return (
-    <div
-      id="itemDetailsContainer"
-      class="oj-flex-item oj-md-6 oj-sm-12"
-    >
+    <div id="itemDetailsContainer" class="oj-flex-item oj-md-6 oj-sm-12">
       <h3>Item Details</h3>
+      {messageState.message + " : "}
       {Array.from(props.item.values())[0]}
+      <br />
+      <oj-button onojAction={updateMessage}>Update Message</oj-button>
     </div>
   );
 };
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,23 @@
 import { Component, ComponentChild, h } from "preact";
 import { ExtendGlobalProps, customElement } from "ojs/ojvcomponent";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit"
+
+const sampleReducer = (state = {message: "Hello, Redux!"}, action) => {
+  // Check to see if the reducer cares about this action
+  if (action.type === 'message/update') {
+    // If so, make a copy of `state`
+    console.log('in the action type of message update')
+    return {
+      ...state,
+      // and update the copy with the new value
+      message: action.payload
+    }
+  }
+  // otherwise return the existing state unchanged
+  return state
+}
+const store = configureStore({ reducer: sampleReducer });
 
 import Content from "./Content/Content";
 import Footer from "./Footer";
@@ -22,9 +40,11 @@ export class App extends Component<ExtendGlobalProps<Props>> {
   render(props: ExtendGlobalProps<Props>): ComponentChild {
     return (
       <div id="appContainer" class="oj-web-applayout-page">
-        <Header appName={props.appName} userLogin={props.userLogin} />
-        <Content />
-        {/* <Footer /> */}
+        <Provider store={store}>
+          <Header appName={props.appName} userLogin={props.userLogin} />
+          <Content />
+          {/* <Footer /> */}
+        </Provider>
       </div>
     );
   }
